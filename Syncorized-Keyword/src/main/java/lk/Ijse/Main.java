dcb9adfbbd19f273688504c59e506e6cc6b46793
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-04-05 12:39:03 +0530

    Synchorinzed-keyword

--- Syncorized-Keyword/src/main/java/lk/Ijse/Main.java
+++ Syncorized-Keyword/src/main/java/lk/Ijse/Main.java
@@ -2,16 +2,37 @@ package lk.Ijse;
 
 class caculation{
     int num;
-    public void increment(){
+    public synchronized void increment(){   /* synchonrized keyword eken wenne real total eka ganna eka */
         num ++;
-        System.out.println("count :"+num);
+
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         caculation calc =new caculation();
-        calc.increment();
-        calc.increment();
+
+       Thread t1 =new Thread(new Runnable() {
+           @Override
+           public void run() {
+               for (int i=1 ;i<=1000 ;i++){
+                 calc.increment();
+               }
+           }
+       });
+
+        Thread t2 =new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=1 ;i<=10000 ;i++){
+                    calc.increment();
+                }
+            }
+        });
+       t1.start();
+       t2.start();
+       t1.join();
+       t2.join();
+        System.out.println("COUNT :" +calc.num);
     }
 }
