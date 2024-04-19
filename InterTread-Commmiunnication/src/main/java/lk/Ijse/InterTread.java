5734b460358529d12e8748626361f991187be386
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-04-19 09:43:01 +0530

    Intertread-Commiunication

---
+++ InterTread-Commmiunnication/src/main/java/lk/Ijse/InterTread.java
@@ -0,0 +1,63 @@
+package lk.Ijse;
+
+class Q{
+    int num;
+
+    public int getNum(int i) {
+        System.out.println("Get :"+num);
+        return num;
+    }
+
+    public void setNum(int num) {
+        System.out.println("Put :"+num);
+        this.num = num;
+    }
+}
+class producer implements Runnable{
+    Q q;
+    public producer(Q q) {
+        this.q=q;
+        Thread t=new Thread(this,"producer");
+        t.start();
+    }
+    public void run() {
+        int i=0;
+        while (true){
+            q.setNum(i++);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+class consumer implements Runnable{
+    Q q;
+    public consumer(Q q) {
+        this.q=q;
+        Thread t=new Thread(this,"producer");
+        t.start();
+    }
+    public void run() {
+        int i=0;
+        while (true){
+            q.getNum(i++);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
+public class InterTread {
+    public static void main(String[] args) {
+        Q q =new Q();
+        new producer(q);
+        new consumer(q);
+
+    }
+}
